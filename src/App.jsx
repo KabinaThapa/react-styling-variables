fe9979cb9524d4b1948eb9524ea5618850ea469e
Kabina Thapa <thapakabina28@gmail.com>
2023-05-23 22:43:20 -0400

    basic javascript and css added

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './Style.css'
+function App() {
+  const box1 = 'Story'
+  const box2= 'Gallery'
+  const box3='GetStarted'
+
+  return (
+    <>
+      <div class="outline">
+        <div class="head">
+          <h1> Dimensions</h1>
+          <div class="b">
+            <h3>{box1}</h3>
+            <h3>{box2}</h3>
+            <h3>{box2}</h3>
+            </div>
+        </div>
+        <div class="a"></div>
+      </div>
+    </>
+  );
+}
+
+export default App
